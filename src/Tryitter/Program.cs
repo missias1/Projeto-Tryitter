a055246508a21bdb9a33a92c6f97d7a66a9db337
Leticia Missias <87737714+missias1@users.noreply.github.com>
2022-12-14 19:33:12 -0300

    feat: altera visibilidade do swagger no program

--- src/Tryitter/Program.cs
+++ src/Tryitter/Program.cs
@@ -79,11 +79,10 @@ namespace Tryitter
         var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
