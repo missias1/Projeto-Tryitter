1363af62fea939f75ac1b8ec693e68bb23cb103a
Leticia Missias <87737714+missias1@users.noreply.github.com>
2022-12-14 13:01:15 -0300

    feat: adiciona geração de token

--- src/Tryitter/Autentication/TokenConstant.cs
+++ src/Tryitter/Autentication/TokenConstant.cs
@@ -1,7 +1,7 @@
 namespace Tryitter.Autentication
 {
-  public static class TokenConstant
-  {
-    public const string Secret = "";
-  }
+    public static class TokenConstant
+    {
+        public const string Secret = "aab23e941b76caba3bebb2fb8b18aecb097166098afdf44b1d64f977972525d6";
+    }
 }
